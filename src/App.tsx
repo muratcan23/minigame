1bc2a2e6e2d40e8398d05fda725322a6fed9b793
Murat Can <muratal0606@gmail.com>
2023-07-09 20:25:15 +0300

    add a description of about page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import { Box, Button, Divider, Flex, Image, Text } from "@chakra-ui/react";
+import {
+  AbsoluteCenter,
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Image,
+  ListItem,
+  OrderedList,
+  Text,
+} from "@chakra-ui/react";
 import { Link, Link as ReactRouterLink } from "react-router-dom";
 
 function App() {
   return (
-    <Flex flexDirection="column">
+    <Flex flexDirection="column" minHeight="100vh">
       <Flex
-        background="#111111"
+        background="#138D69"
         justifyContent="center"
         alignItems="center"
         h="10rem"
@@ -27,13 +37,12 @@ function App() {
           w="100%"
           justifyContent="center"
           alignItems="center"
-          marginBottom=".5rem"
         >
           <Flex
             margin="2px"
-            borderRadius="44%"
+            borderRadius="47%"
             color="#92BED7"
-            _hover={{ background: "#33390C" }}
+            _hover={{ background: "#138D69" }}
           >
             <Button
               as={ReactRouterLink}
@@ -50,28 +59,102 @@ function App() {
           </Flex>
         </Flex>
         <Flex
+          direction={["column", "row"]}
           w="100%"
-          background="black"
+          background="#138d69"
           alignItems="center"
           justifyContent="center"
         >
-          <Flex height="350px" background="#E4AE6C" borderRadius="5%">
-            <Flex w="70%">
-              <Image
-                h="250px"
-                w="50%"
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Warning_sign_font_awesome-red.svg/2048px-Warning_sign_font_awesome-red.svg.png"
-              ></Image>
+          <Flex
+            height="350px"
+            background="#E4AE6C"
+            borderRadius="10px"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Flex mr="auto" my="auto">
+              <Box ml="20px">
+                <Image
+                  h="200px"
+                  w="100%"
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Warning_sign_font_awesome-red.svg/2048px-Warning_sign_font_awesome-red.svg.png"
+                ></Image>
+              </Box>
               <Flex w="100%">
-                <Text fontFamily="monospace" fontWeight="bold" color="black">
-                  Hey, be careful! before You begin. If you are not familiar to
-                  loose do not play with me.
-                </Text>
+                <OrderedList ml="30px">
+                  <ListItem
+                    fontFamily="monospace"
+                    fontWeight="bold"
+                    color="black"
+                  >
+                    <Text
+                      fontFamily="monospace"
+                      fontWeight="bold"
+                      color="black"
+                      fontSize="16px"
+                    >
+                      Hey, be careful! before You begin. If you are not familiar
+                      to loose do not play with me.😡
+                    </Text>
+                  </ListItem>
+                  <ListItem
+                    fontFamily="monospace"
+                    fontWeight="bold"
+                    color="black"
+                  >
+                    <Text
+                      fontFamily="monospace"
+                      fontWeight="bold"
+                      color="black"
+                      fontSize="16px"
+                    >
+                      Uncomfortable with basic math? Skip playing with me.
+                      <Text color="#BD120F" mt="-1px">
+                        Equation: 4 + 4 + 4 + 4 = x, where x = 4/y. Can you
+                        determine the value of y?
+                      </Text>
+                    </Text>
+                  </ListItem>
+                  <ListItem
+                    fontFamily="monospace"
+                    fontWeight="bold"
+                    color="black"
+                  >
+                    <Text
+                      fontFamily="monospace"
+                      fontWeight="bold"
+                      color="black"
+                      fontSize="16px"
+                    >
+                      Keep your expectaions low.😒
+                    </Text>
+                  </ListItem>
+                  <ListItem
+                    fontFamily="monospace"
+                    fontWeight="bold"
+                    color="black"
+                  >
+                    <Text
+                      fontFamily="monospace"
+                      fontWeight="bold"
+                      color="black"
+                      fontSize="16px"
+                    >
+                      Don't be greedy it's just a game... 😊
+                    </Text>
+                  </ListItem>
+                </OrderedList>
               </Flex>
             </Flex>
           </Flex>
         </Flex>
-        <Divider />
+        <Box position="relative" padding="10" bg="green">
+          <Divider h="10px" flex="1" bg="gray" />
+
+          <AbsoluteCenter bg="white" px="4" fontFamily="fantasy">
+            GAME
+          </AbsoluteCenter>
+        </Box>
         <Flex
           background="#8F2C19"
           h="400px"
--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -1,11 +1,44 @@
-function About() {
+import { Box, Center, Flex, Heading, Text } from "@chakra-ui/react";
+
+const About = () => {
+  // Example shuffle method
+
   return (
-    <div>
-      <h1>About Us</h1>
-      <p>This page provides information about our mini-game website.</p>
-      {/* Add any additional information or team details here */}
-    </div>
+    <Flex
+      p={4}
+      bg="gray.200"
+      borderRadius="md"
+      justifyContent="center"
+      alignItems="center"
+      flexDirection="column"
+    >
+      <Center>
+        <Heading as="h1" size="lg" mb={4} color="teal.600">
+          About
+        </Heading>
+      </Center>
+      <Text mb={4} color="gray.700">
+        This mini game API was developed using React and Chakra UI. It provides
+        a set of features for building interactive games.
+      </Text>
+      <Text mb={4} color="gray.700">
+        The mini game included in this API utilizes the shuffle method to add an
+        element of randomness and challenge. The shuffle method is used to
+        randomize the order of elements, creating a dynamic and unpredictable
+        experience for players.
+      </Text>
+      <Text mb={4} color="gray.700">
+        Here's an example of a shuffle method implementation:
+      </Text>
+      <Box p={4} bg="gray.100" borderRadius="md">
+        <Text fontFamily="monospace" fontSize="sm" color="gray.800">
+          {`const shuffleArray = (array) => {
+  // ... implementation of the shuffle method
+};`}
+        </Text>
+      </Box>
+    </Flex>
   );
-}
+};
 
 export default About;
